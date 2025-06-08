fbb9d79f4e0066b0170aea887d4fa5935b3ba4b2
klmhyeonwoo <gentlemonster77@likelion.org>
2025-06-08 13:45:27 +0900

    feat: #42 migration greeting card components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import { Fragment } from "react";
 import AnnounceCard from "@/components/card/AnnounceCard";
 import "@/styles/domain/main.scss";
 import { api } from "@/api";
-import GreetingCard from "@/components/common/greeting-card";
 import greeting_image_eyes from "../../public/images/eyes.gif";
 import greeting_image_waving from "../../public/images/waving.gif";
+import greeting_image_rocket from "../../public/images/rocket.gif";
+import greeting_image_virus from "../../public/images/virus.gif";
 import HomeLogo from "@/components/common/home-logo";
+import GreetingCard from "@/components/common/greeting-card";
 
 export const dynamic = "force-dynamic";
 
@@ -86,16 +88,36 @@ export default async function Home() {
         <HomeLogo />
         <div className="greeting__card__wrapper">
           <GreetingCard
-            title="빅테크 채용 공고 확인하기"
-            description="네카라쿠배 채용 공고를 한 눈에 확인해보세요"
-            image={greeting_image_eyes}
+            title="네카라쿠배 공고 모아보기"
+            subTitle="네카라쿠배 빅테크 채용관"
+            description="네이버, 카카오, 라인, 쿠팡, 배달의 민족 등 국내 빅테크 기업들의 채용 공고를 한 눈에 확인해보세요"
             navigate="web"
+            colorSet={["#16222A", "#3A6073"]}
+            image={greeting_image_eyes}
           />
           <GreetingCard
-            title="서비스 문의하기"
-            description="다양한 서비스들을 준비하고 있어요"
+            title="이용에 불편함이 있으신가요?"
+            subTitle="서비스 문의하기"
+            description="서비스 이용 중 불편함이나 필요한 기능이 있다면 언제든지 문의해 주세요."
+            navigate="web"
+            colorSet={["#1A2980", "#26D0CE"]}
             image={greeting_image_waving}
-            navigate="channel-talk"
+          />
+          <GreetingCard
+            title="취업 및 이직 커뮤니티"
+            subTitle="커뮤니티 참여하기"
+            description="커뮤니티를 통해 여러분들의 생각과 다른 사람들과 이야기하며 다양한 의견을 나누어보세요."
+            navigate="not-yet"
+            colorSet={["#2C3E50", "#4CA1AF"]}
+            image={greeting_image_virus}
+          />
+          <GreetingCard
+            title="더 많은 기능들이 다가오고 있어요"
+            subTitle="로켓처럼 빠르게 발전하고 있어요"
+            navigate="release-notes"
+            description="더 다양한 정보를 위해 지속적으로 발전하고 있으며, 더 많은 기능들이 곧 추가될 예정이에요."
+            colorSet={["#232526", "#414345"]}
+            image={greeting_image_rocket}
           />
         </div>
         <Banner
