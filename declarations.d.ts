6882ce1168e4bd15117e73dc90dbb8a31954360e
umsungjun <99730910+umsungjun@users.noreply.github.com>
2025-08-22 13:08:13 +0900

    fix: add type declarations for GIF, PNG, and SVG to fix TS2307 errors
    - Added declarations.d.ts to define modules for *.gif, *.png, and *.svg
    - Allows importing public folder images without TypeScript errors

--- declarations.d.ts
+++ declarations.d.ts
@@ -2,3 +2,15 @@ declare module "*.mp4" {
   const src: string;
   export default src;
 }
+declare module "*.gif" {
+  const src: string | StaticImageData;
+  export default src;
+}
+declare module "*.png" {
+  const src: string |StaticImageData
+  export default src;
+}
+declare module "*.svg" {
+  const src: string;
+  export default src;
+}
