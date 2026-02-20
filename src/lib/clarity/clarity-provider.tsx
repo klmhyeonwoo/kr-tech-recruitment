acbcb835195b663c64cb0a753c5571f4f39a73f8
klmhyeonwoo <gentlemonster77@likelion.org>
2026-02-20 22:59:41 +0900

    fix: Clarity React Suspense Crash Bug

--- src/lib/clarity/clarity-provider.tsx
+++ src/lib/clarity/clarity-provider.tsx
@@ -1,16 +1,27 @@
 "use client";
-import React, { createContext, PropsWithChildren, useEffect } from "react";
+import React, {
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useRef,
+} from "react";
 import Clarity from "@microsoft/clarity";
 
 export default function ClarityProvider({ children }: PropsWithChildren) {
   // TODO: 추후 관련 상태 값이 추가되면 Context API로 추가 제공
 
   const Provider = createContext({});
+  const initializedRef = useRef(false);
 
   useEffect(() => {
+    if (!initializedRef.current) return;
     // * Clarity.init이 내부적으로 스크립트 로드/비동기 상태를 건드리면서, Suspense 경계 정리 타이밍과 충돌
     // * UseEffect 안에서 실행되어야 함
-    Clarity.init(process.env.NEXT_PUBLIC_CLARITY_KEY as string);
+    const CLARITY_KEY = process.env.NEXT_PUBLIC_CLARITY_KEY;
+    if (!CLARITY_KEY) return;
+
+    Clarity.init(CLARITY_KEY);
+    initializedRef.current = true;
   }, []);
 
   return <Provider value={{}}> {children} </Provider>;
